c7fd3b9d381a3c5c18f46fb2e41077f1a7b6f5b4
haihs-github <haihs.ptit@gmail.com>
2024-10-04 15:39:56 +0700

    update form.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -5,3 +5,23 @@ class TruyenForm(forms.ModelForm):
 	class Meta:
 		model = Truyen
 		fields = ["ten", "theloai", "mota", "tacgia", "anhbia", "anhnen"]
+
+class ChapForm(forms.ModelForm):
+	class Meta:
+		model = Chap
+		fields = ["stt", "ten", "luotxem", "thoigiandang", "truyen"]
+
+class TrangForm(forms.ModelForm):
+	class Meta:
+		model = Trang
+		fields = ["anh", "chap"]
+
+class ThongbaoForm(forms.ModelForm):
+	class Meta:
+		model = Thongbao
+		fields = ["noidung"]
+
+class NguoidungForm(forms.ModelForm):
+	class Meta:
+		model = Thongbao
+		fields =  ["ten", "matkhau", "vaitro", "yeuthich", "lichsu", "thongbao", "truyendang"]
