31d43847cfc0acd604edcde012abf55ed340205c
haihs-github <haihs.ptit@gmail.com>
2024-10-05 14:44:40 +0700

    add dev-fe, add home.html, add index.html, add upload index.html, update views1.py

--- myapp/views1.py
+++ myapp/views1.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, redirect
+from .models import Truyen, Chap, Trang, Thongbao, Nguoidung
 from .forms import TruyenForm
 # Create your views here.
 
+# def sort_by_view_in_month():
+# 	Truyens = Truyen.object.
+
 def home(request):
-	if request.method == 'POST':
-		if 'upload-truyen' in request.POST:
-			form = TruyenForm(request.POST, request.FILES)
-			if form.is_valid():
-				form.save()
-				print('save')
-				return redirect('/admin')
-			else:
-				print('save not done')
-				return render(request, 'home.html', {'form': form})
 	return render(request, 'home.html')
+
+# def upload(request):
+# 	if request.method == 'POST':
+# 		if 'upload-truyen' in request.POST:
+# 			form = TruyenForm(request.POST, request.FILES)
+# 			if form.is_valid():
+# 				form.save()
+# 				print('save')
+# 				return redirect('/admin')
+# 			else:
+# 				print('save not done')
+# 				return render(request, 'home.html', {'form': form})
+# 	return render(request, 'home.html')
