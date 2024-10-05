c4a398f8b09d1d09c72942f2ecbc224f0e285782
haihs-github <haihs.ptit@gmail.com>
2024-10-05 22:19:46 +0700

    done new_update

--- myapp/views1.py
+++ myapp/views1.py
@@ -5,14 +5,30 @@ from datetime import datetime, timedelta
 from django.utils.timezone import make_aware
 # Create your views here.
 
-def top3_by_like():
+# chức năng trang home
+
+def top3_by_like(): # top 3 truyện có lượt yêu thích cao nhất( slider trang home)
 	top3 = Truyen.objects.all().order_by('-luotthich')[:3]
 	return top3
 
-def home(request):
+def new_update():
+	chaps = Chap.objects.all().order_by('-thoigiandang')
+	new_update = list()
+	for x in chaps:
+		if x.truyen not in new_update:
+			new_update.append(x.truyen)
+	new_update = new_update[:12]
+	print(new_update)
+	for x in new_update:
+		print(x.ten)
+	return new_update
+
+def home(request): # view trang home
+	list_new_update = new_update()
 	top3 = top3_by_like()
 	context = {
-		'top3' : top3
+		'top3' : top3,
+		'list_new_update': list_new_update
 	}
 	return render(request, 'home.html', context)
 
